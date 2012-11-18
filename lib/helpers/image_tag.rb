9d96cc047e5e2e1c401ebabf5e03910f47ef0596
Ken Hagler <khagler@orange-road.com>
2012-11-18 15:10:07 -0600

    Fixed image_tag helper
    The helper for creating <img> tags in gallery pages is now working correctly.

---
+++ lib/helpers/image_tag.rb
@@ -0,0 +1,19 @@
+module ImageTagMaker    
+    require 'image_size'
+    require 'pathname'
+
+    def image_tag (item)
+        photo = item[:photoID] + ".jpg"
+        gallery_page = Pathname.new(item.path)
+        # I'm using this method of finding the path to the image file instead
+        # of trying to get the parent of the parent of "item" because that
+        # approach didn't work, and I got tired of struggling with trying to
+        # figure out why.
+        photo_path = gallery_page.parent.parent + "Images" + photo
+        alt_text = "Photo of " + item[:title]
+        img_src = photo_path.relative_path_from(gallery_page)
+        gallery_full_path = Pathname.new(item.rep_named(:default).raw_path)
+        photo_size = ImageSize.path(gallery_full_path.parent.parent + "Images" + photo)
+        return "<img height=\"#{photo_size.height}\" alt=\"#{alt_text}\" src=\"#{img_src}\" width=\"#{photo_size.width}\" border=\"0\" />"
+    end
+end

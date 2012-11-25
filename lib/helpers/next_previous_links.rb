6fda93fa7fe5d2a9321dc649ad38d068060c8526
Ken Hagler <khagler@orange-road.com>
2012-11-24 21:58:01 -0600

    Clean up style
    Bring code style in line with common Ruby style practices.

--- lib/helpers/next_previous_links.rb
+++ lib/helpers/next_previous_links.rb
@@ -1,30 +1,30 @@
 module NextPreviousLinks
-    def next_link
-        siblings = find_siblings(@item)
-        if siblings.size == 1
-            # There's only one picture in this gallery, so no need for
-            # next and previous links.
-            return
-        end
-        siblings.sort! { |a, b| a[:gallery_order] <=> b[:gallery_order] }
-        next_index = siblings.index(@item) + 1
-        if siblings[next_index].nil?
-            # If we're on the last page of the gallery, "next" will wrap
-            # around to the first.
-            next_index = 0
-        end
-        return "Next: " + link_to(siblings[next_index][:title], siblings[next_index])
+  def next_link
+    siblings = find_siblings(@item)
+    if siblings.size == 1
+      # There's only one picture in this gallery, so no need for
+      # next and previous links.
+      return
     end
+    siblings.sort! { |a, b| a[:gallery_order] <=> b[:gallery_order] }
+    next_index = siblings.index(@item) + 1
+    if siblings[next_index].nil?
+      # If we're on the last page of the gallery, "next" will wrap
+      # around to the first.
+      next_index = 0
+    end
+    link_to("Next", siblings[next_index]) + ": #{siblings[next_index][:title]}"
+  end
   
-    def previous_link
-        siblings = find_siblings(@item)
-        if siblings.size == 1
-            # There's only one picture in this gallery, so no need for
-            # next and previous links.
-            return
-        end
-        siblings.sort! { |a, b| a[:gallery_order] <=> b[:gallery_order] }
-        previous_index = siblings.index(@item) - 1
-        return "Previous: " + link_to(siblings[previous_index][:title], siblings[previous_index])
+  def previous_link
+    siblings = find_siblings(@item)
+    if siblings.size == 1
+      # There's only one picture in this gallery, so no need for
+      # next and previous links.
+      return
     end
+    siblings.sort! { |a, b| a[:gallery_order] <=> b[:gallery_order] }
+    previous_index = siblings.index(@item) - 1
+    link_to("Previous", siblings[previous_index]) + ": #{siblings[previous_index][:title]}"
+  end
 end
--- lib/helpers/utils.rb
+++ lib/helpers/utils.rb
@@ -1,9 +1,9 @@
 module Utilities
     
-    # Takes an item as the parameter and returns an array of items located
-    # in the same folder.
-    def find_siblings (item)
-        parent_identifier = item.identifier.sub(/[^\/]+\/$/, '')
-        return @items.select { |e| e.identifier.include? parent_identifier }
-    end
+  # Takes an item as the parameter and returns an array of items located
+  # in the same folder.
+  def find_siblings (item)
+    parent_identifier = item.identifier.sub(/[^\/]+\/$/, '')
+    @items.select { |e| e.identifier.include? parent_identifier }
+  end
 end

eceecbe97b7086c4efce23029c50745b3a260347
Ken Hagler <khagler@orange-road.com>
2012-12-02 21:47:43 -0600

    Rewrite image_tag to be general
    Code was repeated between image_tag and make_gallery, so I rewrite image_tag to be generally useful so that it can be called from within make_gallery.

--- lib/helpers/image_tag.rb
+++ lib/helpers/image_tag.rb
@@ -1,19 +1,38 @@
 require 'image_size'
 require 'pathname'
 
-module ImageTagMaker    
-  def image_tag
-    photo = @item[:photoID] + ".jpg"
-    gallery_page = Pathname.new(@item.path)
-    # I'm using this method of finding the path to the image file instead
-    # of trying to get the parent of the parent of "@item" because that
-    # approach didn't work, and I got tired of struggling with trying to
-    # figure out why.
-    photo_path = gallery_page.parent + "Images" + photo
-    alt_text = "Photo of " + @item[:title]
-    img_src = photo_path.relative_path_from(gallery_page)
-    gallery_full_path = Pathname.new(@item.rep_named(:default).raw_path)
-    photo_size = ImageSize.path(gallery_full_path.parent.parent + "Images" + photo)
+module ImageTagMaker
+  
+  # The +image_tag+ method creates an HTML img tag.
+  # === Parameters
+  # * _photo_ = The file name of the photo
+  # * _alt_text_ = The string to use as the images alt text
+  def image_tag(photo, alt_text)
+    images = 'Images'
+    # The path within the nanoc site. Its root is the "content" folder.
+    item_path = Pathname.new(@item.path)
+    # The full path on the file system.
+    item_full_path = Pathname.new(@item.rep_named(:default).raw_path)
+
+    # Figuring out where exactly the photo is is complicated by the need to
+    # maintain backward compatibility with the existing site structure, where
+    # images can be in any of three different places:
+    # 1. In '../Images/' for full-size photos on photo pages
+    # 2. In './Images/' for thumbnails on gallery indices
+    # 3. In '/img/' for everything else.
+    if (photo_full_path = item_full_path.parent.parent.join(images, photo)).exist?
+      photo_path = item_path.parent.join(images, photo)
+    elsif (photo_full_path = item_full_path.parent.join(images, photo)).exist?
+      photo_path = item_path.join(images, photo)
+    else
+      photo_path = Pathname.new('/img').join(photo)
+      # We have to get the path to '/img' this way because the directory has
+      # no index.html, so as far as nanoc is concerned '/img/' doesn't exist.
+      photo_full_path = Pathname.new(@items.find { |e| e.identifier == '/' } \
+        .rep_named(:default).raw_path).parent.join('img', photo)
+    end
+    img_src = photo_path.relative_path_from(item_path)    
+    photo_size = ImageSize.path(photo_full_path)
     %(<img height="#{photo_size.height}" alt="#{alt_text}" src="#{img_src}" width="#{photo_size.width}" border="0" />)
   end
 end

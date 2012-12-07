4b7716a5556b8835dd324bb8d68c396ec22e9d44
Ken Hagler <khagler@orange-road.com>
2012-12-06 22:37:40 -0600

    Add more information to the repo list
    Now the list is a definition list showing the name of the repo, its description, and if it's a fork.

--- lib/helpers/github_repos.rb
+++ lib/helpers/github_repos.rb
@@ -14,9 +14,11 @@ module GitHubRepos
       request = Net::HTTP::Get.new uri.request_uri
       @response = http.request request
     end
-    @repo_list = "<ul>\n"
-    JSON.parse(@response.body,
-               {:symbolize_names => true}).each { |e| @repo_list << %(<li>#{e[:full_name]}</li>\n) }
-    @repo_list << "</ul>\n"
+    @repo_list = "<dl>\n"
+    JSON.parse(@response.body, {:symbolize_names => true}).each do |e|
+      @repo_list << %(<dt>#{e[:full_name]} #{'(Fork)' if e[:fork]}</dt>\n)
+      @repo_list << %(<dd>#{e[:description]}</dd>\n)
+    end
+    @repo_list << "</dl>\n"
   end
 end

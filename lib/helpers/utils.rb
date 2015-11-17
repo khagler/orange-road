64b7470c7f921a3241ce0ed40431d27f4497faa7
Ken Hagler <khagler@orange-road.com>
2015-11-17 09:59:17 -0600

    Update for nanoc 4
    There were some changes needed in the config file and in the way identifiers are used to work with nanoc 4.

--- lib/helpers/utils.rb
+++ lib/helpers/utils.rb
@@ -3,7 +3,7 @@ module Utilities
   # Takes an item as the parameter and returns an array of items located
   # in the same folder.
   def find_siblings (item)
-    parent_identifier = item.identifier.sub(/[^\/]+\/$/, '')
-    @items.select { |e| e.identifier.include? parent_identifier }
+    parent_identifier = item.identifier.to_s.sub(/[^\/]+\/$/, '')
+    @items.select { |e| e.identifier.to_s.include? parent_identifier }
   end
 end
